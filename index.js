87643f5cfb1146731d2bd5bbe7df06f21f2dc512
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-07-12 10:32:55 +0200

    Added a catch for default background img and author

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=;hjksdf;kljsdfgl;kdsjfgljksdfglkjhsdfg")
-    .then(res => res.json())
-    .then(data => {
-        co
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-
-
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.urls.json);
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
+  })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjA3NzMwMjV8&ixlib=rb-4.0.3&q=80&w=1080)`;
+    document.getElementById("author").textContent = `By: OC Gonzalez
+`;
+  });
